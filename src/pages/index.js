8a2efb4e386c97297ea13ea2ee02d79b4f1784ce
Paramveer <paramveermarwahafc@gmail.com>
2025-08-06 21:57:37 +0200

    feat: pages imports via single file

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as HomeLayout } from "./HomeLayout";
+export { default as Landing } from "./Landing";
+export { default as Error } from "./Error";
+export { default as Login } from "./Login";
+export { default as Register } from "./Register";
+export { default as DashboardLayout } from "./DashboardLayout";
