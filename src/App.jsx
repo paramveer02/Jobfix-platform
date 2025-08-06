5f6781076baf4c4ff966cfea7cbf5527e4ad1193
Paramveer <paramveermarwahafc@gmail.com>
2025-08-06 21:11:43 +0200

    feat: integrated browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-const App = () => {
-  return <h1>JobFix App</h1>;
-};
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomeLayout } from "./pages/HomeLayout";
+import { About } from "./pages/About";
 
+const router = createBrowserRouter([
+  { path: "/", element: <HomeLayout /> },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 export default App;
