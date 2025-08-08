bc5f1163446ca7821481f1fe48ae9a9196051bb1
Paramveer <paramveermarwahafc@gmail.com>
2025-08-08 12:41:35 +0200

    feat: added additional components (in progress)

---
+++ src/components/SmallSidebar.jsx
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+
+const SmallSidebar = () => {
+  return <Wrapper>SmallSidebar</Wrapper>;
+};
+export default SmallSidebar;
