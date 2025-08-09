f36b6d77459748b1ceece7ac38cbffa5bb5ebff5
Paramveer <paramveermarwahafc@gmail.com>
2025-08-09 19:22:17 +0200

    feat: added logout functionality

---
+++ src/components/LogoutContainer.jsx
@@ -0,0 +1,30 @@
+import { useContext, useState } from "react";
+import Wrapper from "../assets/wrappers/LogoutContainer";
+import { DashboardContext } from "../context/DashboardContext";
+import { FaCaretDown, FaUserCircle } from "react-icons/fa";
+FaUserCircle;
+
+const LogoutContainer = () => {
+  const [showLogout, setShowLogout] = useState(false);
+  const { user, logoutUser } = useContext(DashboardContext);
+
+  return (
+    <Wrapper>
+      <button
+        type="button"
+        className="btn logout-btn"
+        onClick={() => setShowLogout(!showLogout)}
+      >
+        <FaUserCircle />
+        {user?.name}
+        <FaCaretDown />
+      </button>
+      <div className={showLogout ? "dropdown show-dropdown" : "dropdown"}>
+        <button type="button" className="dropdown-btn" onClick={logoutUser}>
+          logout
+        </button>
+      </div>
+    </Wrapper>
+  );
+};
+export default LogoutContainer;
