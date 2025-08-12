ac18e4c67b69400adf926a57452d6e0464800177
Paramveer <paramveermarwahafc@gmail.com>
2025-08-12 10:11:32 +0200

    feature: updated Add Job component

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,4 +1,35 @@
+import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import { FormRow } from "../components";
+import Wrapper from "../assets/wrappers/AddJob";
+
 const AddJob = () => {
-  return <div>AddJob</div>;
+  const { user } = useOutletContext();
+  const navigate = useNavigation();
+  const isSubmitting = navigate.state === "submitting";
+
+  return (
+    <Wrapper>
+      <Form method="post" className="form">
+        <h4 className="form-title">Add Job</h4>
+        <div className="form-center">
+          <FormRow type="text" name="position" />
+          <FormRow type="text" name="company" />
+          <FormRow
+            type="text"
+            labelText="job location"
+            name="jobLocation"
+            defaultValue={user.location}
+          />
+        </div>
+      </Form>
+      <button
+        type="submit"
+        className="btn btn-block form-btn  "
+        disabled={isSubmitting}
+      >
+        {isSubmitting ? "submitting..." : "submit"}
+      </button>
+    </Wrapper>
+  );
 };
 export default AddJob;
