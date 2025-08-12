16ee46ea9c426c14c6004a8ca5e0a64b5aa66fad
Paramveer <paramveermarwahafc@gmail.com>
2025-08-12 10:08:27 +0200

    fix: updated the main logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,4 @@
-import logo from "../assets/images/logo.png";
-// import logo from "../assets/images/logo.svg";
+import logo from "../assets/images/logo1.png";
 
 const Logo = () => {
   return <img src={logo} alt="jobfix logo" className="logo" />;
