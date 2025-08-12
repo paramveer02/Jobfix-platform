c27f15b1c576070c0f7201bfa109791699a3aec4
Paramveer <paramveermarwahafc@gmail.com>
2025-08-12 13:07:56 +0200

    feature: form component to create job

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -1,6 +1,29 @@
-import { Form, useNavigation, useOutletContext } from "react-router-dom";
+import {
+  Form,
+  redirect,
+  useNavigation,
+  useOutletContext,
+} from "react-router-dom";
 import { FormRow } from "../components";
+import { FormRowSelect } from "../components";
 import Wrapper from "../assets/wrappers/AddJob";
+import { JOB_STATUS, JOB_TYPE } from "../../../jobfix-server/utils/constants";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/jobs", data);
+    toast.success("Job added successfully");
+    return redirect("all-jobs");
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+    return error;
+  }
+};
 
 const AddJob = () => {
   const { user } = useOutletContext();
@@ -20,15 +43,29 @@ const AddJob = () => {
             name="jobLocation"
             defaultValue={user.location}
           />
+
+          <FormRowSelect
+            name="jobStatus"
+            labelText=" Job Status"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            name="jobType"
+            labelText=" Job Type"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
+          />
+
+          <button
+            type="submit"
+            className="btn btn-block form-btn  "
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? "submitting..." : "submit"}
+          </button>
         </div>
       </Form>
-      <button
-        type="submit"
-        className="btn btn-block form-btn  "
-        disabled={isSubmitting}
-      >
-        {isSubmitting ? "submitting..." : "submit"}
-      </button>
     </Wrapper>
   );
 };
