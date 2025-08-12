88b50771da39ef3f3b9ed81c7ee7300dceab6bf3
Paramveer <paramveermarwahafc@gmail.com>
2025-08-12 10:09:03 +0200

    feature: added action method to handle form submission

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { Form, Link, redirect } from "react-router-dom";
 import { Logo, FormRow } from "../components";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  try {
+    await customFetch.post("/auth/login", data);
+    toast.success("Successfully logged in");
+    return redirect("/dashboard");
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+    return error;
+  }
+};
 
 const Login = () => {
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Login</h4>
         <FormRow type="text" name="email" defaultValue="param@test.com" />
@@ -22,7 +38,7 @@ const Login = () => {
             Register
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,31 @@
-import { Link } from "react-router-dom";
+import { Form, Link, redirect, useNavigation } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { Logo, FormRow } from "../components";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ request }) => {
+  const formdata = await request.formData();
+  const data = Object.fromEntries(formdata);
+
+  try {
+    await customFetch.post("/auth/signup", data);
+    toast.success("Registration Successful");
+    return redirect("/login");
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+    console.log(error);
+    return error;
+  }
+};
 
 const Register = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   return (
     <Wrapper>
-      <form className="form">
+      <Form method="post" className="form">
         <Logo />
         <h4>Register</h4>
         <FormRow type="text" name="name" defaultValue="Paramvir" />
@@ -18,8 +38,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="India" />
         <FormRow type="text" name="email" defaultValue="param@test.com" />
         <FormRow type="password" name="password" defaultValue="user123!" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
         </button>
         <p>
           Already a member?
@@ -27,7 +47,7 @@ const Register = () => {
             Login
           </Link>
         </p>
-      </form>
+      </Form>
     </Wrapper>
   );
 };
