b6e277b4670d2a06f7e66c91f8005f9f240821fe
Paramveer <paramveermarwahafc@gmail.com>
2025-08-13 18:33:30 +0200

    feature: page to delete jobs

---
+++ src/pages/DeleteJob.jsx
@@ -0,0 +1,13 @@
+import { redirect } from "react-router-dom";
+import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
+
+export const action = async ({ params }) => {
+  try {
+    await customFetch.delete(`/jobs/${params.id}`);
+    toast.success("Job deleted successfully");
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+  }
+  return redirect("/dashboard/all-jobs");
+};
