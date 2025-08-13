3ba73d0905a5b314edd1ba1da569908c219cf6db
Paramveer <paramveermarwahafc@gmail.com>
2025-08-13 18:33:57 +0200

    feature: page to display all jobs integrated with loader

--- src/pages/AllJobs.jsx
+++ src/pages/AllJobs.jsx
@@ -1,5 +1,31 @@
-const AllJobs = () => {
-  return <div>AllJobs</div>;
+import { toast } from "react-toastify";
+import customFetch from "../utils/customFetch";
+import { useLoaderData } from "react-router-dom";
+import { JobsContainer, SearchContainer } from "../components";
+import { createContext, useContext } from "react";
+
+export const loader = async () => {
+  try {
+    const { data } = await customFetch.get("/jobs");
+    return data;
+  } catch (error) {
+    toast.error(error?.response?.data?.message);
+    return error;
+  }
 };
+
+const AllJobsContext = createContext();
+
+const AllJobs = () => {
+  const { data } = useLoaderData();
+  return (
+    <AllJobsContext.Provider value={{ data }}>
+      <SearchContainer />
+      <JobsContainer />
+    </AllJobsContext.Provider>
+  );
+};
+
+export const UseAllJobsContext = () => useContext(AllJobsContext);
 
 export default AllJobs;
