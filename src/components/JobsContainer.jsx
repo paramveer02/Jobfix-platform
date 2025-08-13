71f0e66c72c17e72b4be1346e9c03a4ea0fadf8d
Paramveer <paramveermarwahafc@gmail.com>
2025-08-13 18:37:19 +0200

    feature: pages to render jobs along with their actions and loaders

---
+++ src/components/JobsContainer.jsx
@@ -0,0 +1,27 @@
+import Wrapper from "../assets/wrappers/JobsContainer";
+import { UseAllJobsContext } from "../pages/AllJobs";
+import Job from "./Job";
+
+const JobsContainer = () => {
+  const { data } = UseAllJobsContext();
+  const { jobs } = data;
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No Jobs to display...</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <div className="jobs">
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+export default JobsContainer;
