f31aedc6348c9264b9dac6e5cc79a173eb323d67
Paramveer <paramveermarwahafc@gmail.com>
2025-08-16 14:41:24 +0200

    fix(form): forward props to select so query params submit on change

--- src/components/FormRowSelect.jsx
+++ src/components/FormRowSelect.jsx
@@ -1,22 +1,31 @@
-const FormRowSelect = ({ name, labelText, defaultValue = "", list }) => {
+// components/FormRowSelect.jsx
+export default function FormRowSelect({
+  name,
+  labelText,
+  list = [],
+  defaultValue = "",
+  onChange,
+  ...rest
+}) {
   return (
     <div className="form-row">
       <label htmlFor={name} className="form-label">
         {labelText || name}
       </label>
       <select
-        name={name}
         id={name}
-        className="form-select"
+        name={name}
         defaultValue={defaultValue}
+        onChange={onChange}
+        className="form-select"
+        {...rest}
       >
-        {list.map((itemValue) => (
-          <option key={itemValue} value={itemValue}>
-            {itemValue}
+        {list.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
           </option>
         ))}
       </select>
     </div>
   );
-};
-export default FormRowSelect;
+}
